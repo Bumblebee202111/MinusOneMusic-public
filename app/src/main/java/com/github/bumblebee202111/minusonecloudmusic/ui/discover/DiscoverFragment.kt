9d4c18171724c83ab6e1abbc513437a6f6f726f8
Bumblebee202111 <16421203+bumblebee202111@user.noreply.gitee.com>
2025-12-21 17:18:09 +0800

    refactor(discover): Migrate Discover screen and top bar to Jetpack Compose
    Sync from private repo at commit: b1d6f71d865cf4aeeb7eb8ca269e1cb4e3a99cfd

--- app/src/main/java/com/github/bumblebee202111/minusonecloudmusic/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/github/bumblebee202111/minusonecloudmusic/ui/discover/DiscoverFragment.kt
@@ -1,39 +1,27 @@
 package com.github.bumblebee202111.minusonecloudmusic.ui.discover
 
-import DiscoverList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.recyclerview.widget.DividerItemDecoration
-import com.github.bumblebee202111.minusonecloudmusic.MainActivity
-import com.github.bumblebee202111.minusonecloudmusic.databinding.FragmentDiscoverBinding
 import com.github.bumblebee202111.minusonecloudmusic.model.DiscoverBlock
-import com.github.bumblebee202111.minusonecloudmusic.ui.common.repeatWithViewLifecycle
 import com.github.bumblebee202111.minusonecloudmusic.ui.navigation.DailyRecommendRoute
 import com.github.bumblebee202111.minusonecloudmusic.ui.navigation.NavigationManager
 import com.github.bumblebee202111.minusonecloudmusic.ui.navigation.PlaylistRoute
 import com.github.bumblebee202111.minusonecloudmusic.ui.navigation.SearchRoute
 import com.github.bumblebee202111.minusonecloudmusic.ui.navigation.TopListsRoute
+import com.github.bumblebee202111.minusonecloudmusic.ui.theme.DolphinTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class DiscoverFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = DiscoverFragment()
-    }
-
-    private lateinit var binding: FragmentDiscoverBinding
-    val viewModel: DiscoverViewModel by viewModels()
-
+    private val viewModel: DiscoverViewModel by viewModels()
     @Inject
     lateinit var navigationManager: NavigationManager
 
@@ -41,51 +29,33 @@ class DiscoverFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentDiscoverBinding.inflate(inflater, container, false).apply {
-            lifecycleOwner = viewLifecycleOwner
-        }
+        return ComposeView(requireContext()).apply {
+            setContent {
+                DolphinTheme {
+                    DiscoverScreen(
+                        viewModel = viewModel,
+                        onMenuClick = { navigationManager.openDrawer() },
+                        onSearchClick = { navigationManager.navigate(SearchRoute) },
+                        onDragonBallClick = { type ->
+                            when (type) {
+                                DiscoverBlock.DragonBalls.DragonBall.Type.SONG_RCMD -> navigationManager.navigate(
+                                    DailyRecommendRoute
+                                )
 
-        return binding.root
-    }
+                                DiscoverBlock.DragonBalls.DragonBall.Type.RANK_LIST -> navigationManager.navigate(
+                                    TopListsRoute
+                                )
 
+                                DiscoverBlock.DragonBalls.DragonBall.Type.PRIVATE_FM -> {
+                                }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.topAppBar.apply {
-            setNavigationOnClickListener {
-                navigationManager.openDrawer()
-            }
-            setOnMenuItemClickListener {
-                navigationManager.navigate(SearchRoute)
-                true
-            }
-        }
-
-        binding.blockList.setContent {
-            val blocks by viewModel.blocks.collectAsStateWithLifecycle()
-
-            blocks?.let {
-                DiscoverList(
-                    blocks = it,
-                    onDragonBallClick = { type ->
-                        when (type) {
-                            DiscoverBlock.DragonBalls.DragonBall.Type.SONG_RCMD -> navigationManager.navigate(
-                                DailyRecommendRoute
-                            )
-
-                            DiscoverBlock.DragonBalls.DragonBall.Type.RANK_LIST -> navigationManager.navigate(
-                                TopListsRoute
-                            )
-
-                            DiscoverBlock.DragonBalls.DragonBall.Type.PRIVATE_FM -> {
+                                DiscoverBlock.DragonBalls.DragonBall.Type.PLAYLIST_COLLECTION -> {
+                                }
                             }
-
-                            DiscoverBlock.DragonBalls.DragonBall.Type.PLAYLIST_COLLECTION -> {
-                            }
-                        }
-                    },
-                    onPlaylistClick = ::navigateToPlaylist
-                )
+                        },
+                        onPlaylistClick = ::navigateToPlaylist
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/github/bumblebee202111/minusonecloudmusic/ui/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/github/bumblebee202111/minusonecloudmusic/ui/discover/DiscoverViewModel.kt
@@ -8,12 +8,13 @@ import androidx.media3.common.util.UnstableApi
 import com.github.bumblebee202111.minusonecloudmusic.data.repository.DiscoverRepository
 import com.github.bumblebee202111.minusonecloudmusic.utils.stateInUi
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 import javax.inject.Inject
 
 @HiltViewModel
 class DiscoverViewModel @Inject constructor(
     discoverRepository: DiscoverRepository
 ) : ViewModel() {
-    val blocks = discoverRepository.getDiscoverBlocks().map { it.data }.stateInUi(emptyList())
+    val blocks =
+        discoverRepository.getDiscoverBlocks().mapNotNull { it.data }.stateInUi(emptyList())
 }
