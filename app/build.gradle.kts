44ab9d96d0235b9f961f7ccbe45f4d51727e639e
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2026-02-17 03:05:04 +0800

    chore(deps): Update dependencies
    GitOrigin-RevId: a24f8ef9a98169dba9822077d5df4088de038965

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.ksp)
-    alias(libs.plugins.navigation.safeargs)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.kotlin.compose)
