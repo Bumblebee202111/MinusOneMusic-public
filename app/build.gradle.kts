f4f11ea72175b75717c2fe38ff57951e7df8e4fb
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2026-05-24 17:59:07 +0800

    chore(version): bump version to 1.0.0-beta07
    GitOrigin-RevId: b5ece5440d43912e5ba479dadb7b53a2df525218

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.github.bumblebee202111.minusonecloudmusic"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1000005
-        versionName = "1.0.0-beta06"
+        versionCode = 1000006
+        versionName = "1.0.0-beta07"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
@@ -83,6 +83,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
